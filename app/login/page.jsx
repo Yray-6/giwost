cef7d8328971e8d94a44058b9f4a292cf31d921f
unknown <vwaireonobrudu@gmail.com>
2024-07-19 10:20:30 +0100

    forgot password removed

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -103,13 +103,13 @@ export default function LoginPage() {
             </div>
           </div>
 
-          <div className="flex items-center justify-between">
+          {/* <div className="flex items-center justify-between">
             <div className="text-sm">
               <Link href="/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
                 Forgot your password?
               </Link>
             </div>
-          </div>
+          </div> */}
 
           <div>
             <button
